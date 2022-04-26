a4e2d9517db5185fa50bb0a18c2f9b13ab826ce3
Aren-Gevorgyan <areng4518@gmail.com>
2022-04-26 09:29:19 +0400

    delete option pai

--- routers/optionRouter.js
+++ routers/optionRouter.js
@@ -6,6 +6,6 @@ const optionController = require("../controllers/optionController");
 optionRouter.post("/create", jsonParser, optionController.createOption);
 optionRouter.put("/:id", jsonParser, optionController.upgradeOption);
 optionRouter.delete("/:id", optionController.deleteOption);
-optionRouter.get("/", optionController.getOption);
+// optionRouter.get("/", optionController.getOption);
 
 module.exports = optionRouter;
